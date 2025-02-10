045d4f8033fc65e62475dbdf020cf4b74bbb05c1
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-10 16:50:28 +0100

    log email

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL:", process.env.EMAIL);
+console.log("EMAIL_PASSWORD:", process.env.EMAIL_PASSWORD ? "Loaded" : "Not Loaded");
 
 
 const app = express();
