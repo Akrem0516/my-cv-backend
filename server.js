2e105d5e9bd6407f1cc67275811edc183b27f01c
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-10 11:49:36 +0100

    remove serving files

--- server.js
+++ server.js
@@ -15,9 +15,3 @@ app.listen(PORT , () => {
 app.use(cors());
 app.use(express.json());
 
-// Serve React static files
-app.use(express.static(path.join(__dirname, "../my-cv/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../my-cv/build", "index.html"));
-});
